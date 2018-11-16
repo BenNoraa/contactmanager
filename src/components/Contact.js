82491412932180947fb3443cd609b41de9211126
noraa <bendicksonaaron@gmail.com>
2018-11-16 00:03:01 -0500

    new branch

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,69 +1,63 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { Consumer } from "../context";
 
 class Contact extends Component {
-  // propTypes can be placed here within the Class or down below outside
-  // of the Class where they are currently commented out
-  // static propTypes = {
-  //   contact: PropTypes.object.isRequired
-  // };
-  state = {};
+  state = {
+    showContactInfo: false
+  };
 
-  // how to make a dynamic onclick custom method
-  onShowClick = (contact, e) => {
-    console.log(contact.name);
+  onDeleteClick = (id, dispatch) => {
+    dispatch({ type: "DELETE_CONTACT", payload: id });
   };
 
   render() {
-    // Destructuring example b/c of redundant props.name, props.email, props.phone that existed in each Contact
-    // they are not commented out due to the destructuring seen right below this line.
-    const { contact } = this.props;
-    // above could also be this as seen on the line below where we are pulling attributes from contact like getting contact from props above.
-    // const { name, email, phone } = this.props.contact;
-    return (
-      // Since this is a Class component, we access props by using the 'this' keyword
-      // this is different vs how we access props in the Header component b/c it's not a Class component
-      // Before destructuring
-      //   <div>
-      //     <h4>{this.props.name}</h4>
-      //     <ul>
-      //       <li>Email: {this.props.email}</li>
-      //       <li>Phone: {this.props.phone}</li>
-      //     </ul>
-      //   </div>
+    const { id, name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
 
-      // After destructuring and removing redundant this.props and putting it into a const
-      <div className="card card-body mb-3">
-        <h4>
-          {contact.name}{" "}
-          {/* 'this' keyword is used b/c it is referrring to a method that's in this Class */}
-          <i
-            onClick={this.onShowClick.bind(this, contact)}
-            className="fas fa-sort-down"
-          />
-        </h4>
-        <ul className="list-group">
-          <li className="list-group-item">
-            Email:
-            {contact.email}
-          </li>
-          <li className="list-group-item">
-            Phone:
-            {contact.phone}
-          </li>
-        </ul>
-      </div>
+    return (
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card card-body mb-3">
+              <h4>
+                {name}{" "}
+                <i
+                  onClick={() =>
+                    this.setState({
+                      showContactInfo: !this.state.showContactInfo
+                    })
+                  }
+                  className="fas fa-sort-down"
+                  style={{ cursor: "pointer" }}
+                />
+                <i
+                  className="fas fa-times"
+                  style={{ cursor: "pointer", float: "right", color: "red" }}
+                  onClick={this.onDeleteClick.bind(this, id, dispatch)}
+                />
+              </h4>
+              {showContactInfo ? (
+                <ul className="list-group">
+                  <li className="list-group-item">
+                    Email:
+                    {email}
+                  </li>
+                  <li className="list-group-item">
+                    Phone:
+                    {phone}
+                  </li>
+                </ul>
+              ) : null}
+            </div>
+          );
+        }}
+      </Consumer>
     );
   }
 }
 
-// propTypes defined OUTSIDE of the Class shown here
-
-// Contact.propTypes = {
-//   name: PropTypes.string.isRequired,
-//   email: PropTypes.string.isRequired,
-//   phone: PropTypes.string.isRequired
-// };
 Contact.propTypes = {
   contact: PropTypes.object.isRequired
 };
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,41 +1,22 @@
 import React, { Component } from "react";
 import Contact from "./Contact";
+import { Consumer } from "../context";
 
 class Contacts extends Component {
-  // if you're only using a constructor to create your state you don't need to use a constructor
-  state = {
-    contacts: [
-      {
-        id: 1,
-        name: "John Doe",
-        email: "jdoe@contacts.com",
-        phone: "555-555-5555"
-      },
-      {
-        id: 2,
-        name: "Karen Williams",
-        email: "karen@contacts.com",
-        phone: "333-555-5555"
-      },
-      {
-        id: 3,
-        name: "Henry Johnson",
-        email: "henry@contacts.com",
-        phone: "222-222-2222"
-      }
-    ]
-  };
-
   render() {
-    // Destructuring by pulling the contacts out of the state
-    const { contacts } = this.state;
-
     return (
-      <div>
-        {contacts.map(contact => (
-          <Contact key={contact.id} contact={contact} />
-        ))}
-      </div>
+      <Consumer>
+        {value => {
+          const { contacts } = value;
+          return (
+            <React.Fragment>
+              {contacts.map(contact => (
+                <Contact key={contact.id} contact={contact} />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
