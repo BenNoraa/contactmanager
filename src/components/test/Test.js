93dd287366c74ed935d11d9f49f16c5fc67c2d21
noraa <bendicksonaaron@gmail.com>
2018-11-17 18:48:30 -0500

    lifecycle tests and notes

---
+++ src/components/test/Test.js
@@ -0,0 +1,39 @@
+// import React, { Component } from "react";
+
+// class Test extends Component {
+//   // usually where you make calls to an API and AJAX calls
+//   componentDidMount() {
+//     console.log("componentDidMount...");
+//   }
+
+//   //   // this runs before DidMount
+//   //   componentWillMount() {
+//   //     console.log("componentWillMount...");
+//   //   }
+
+//   // only runs when component updates, i.e., state changes
+//   // if you add this method to context.js you will see this message in the console since that's a state change.
+//   componentDidUpdate() {
+//     console.log("componentDidUpdate...");
+//   }
+
+//   //   componentWillUpdate() {}
+
+//   //   // when your compoonent receives new properits, then this will run
+//   //   // this is usually used with Redux
+//   //   componentWillReceiveProps(nextProps, nextState) {}
+
+//   static getDerivedStateFromProps(nextProps, prevState) {
+//     return null;
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Test Component</h1>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Test;
