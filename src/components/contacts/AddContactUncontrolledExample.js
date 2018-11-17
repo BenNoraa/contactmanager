647720343a8932f262ff2ba663009342eda821fa
noraa <bendicksonaaron@gmail.com>
2018-11-17 13:26:46 -0500

    added uncontrolled form example file

---
+++ src/components/contacts/AddContactUncontrolledExample.js
@@ -0,0 +1,91 @@
+import React, { Component } from "react";
+
+/* this is another AddContact example that shows how we don't connect each input with state but 
+instead are creating a ref on each one so that we can grab onto it inside the submite and get the 
+new data often used in Redux and to bring in props to use as default values and we 
+need a way to submit form and update
+ */
+
+class AddContact extends Component {
+  constructor(props) {
+    super(props);
+
+    this.nameInput = React.createRef();
+    this.emailInput = React.createRef();
+    this.phoneInput = React.createRef();
+  }
+
+  // submitting a new contact method
+  onSubmit = e => {
+    // prevents any default submitting of a form
+    e.preventDefault();
+    const contact = {
+      name: this.nameInput.current.value,
+      email: this.emailInput.current.value,
+      phone: this.phoneInput.current.value
+    };
+
+    console.log(contact);
+  };
+
+  static defaultProps = {
+    name: "Fred Smith",
+    email: "f@test.com",
+    phone: "222-222-2222"
+  };
+
+  render() {
+    // example of getting values from props instead of state
+    // need to use defaultValue on each input vs value which is used with state
+    const { name, email, phone } = this.props;
+    return (
+      <div className="card mb-3">
+        <div className="card-header">Add Contact</div>
+        <div className="card-body">
+          <form onSubmit={this.onSubmit}>
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                name="name"
+                className="form-control form-control-lg"
+                placeholder="Enter Name..."
+                defaultValue={name}
+                ref={this.nameInput}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                className="form-control form-control-lg"
+                placeholder="Enter Email..."
+                defaultValue={email}
+                ref={this.phoneInput}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="name">Phone</label>
+              <input
+                type="text"
+                name="phone"
+                className="form-control form-control-lg"
+                placeholder="Enter Phone..."
+                defaultValue={phone}
+                ref={this.phoneInput}
+              />
+            </div>
+            <input
+              type="submit"
+              value="Add Contact"
+              className="btn btn-light btn-block"
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AddContact;
